37c92e0aabd6fc1457367881f7501d78513c7388
pipxy <polarbearclimatechange2021@protonmail.com>
2022-06-30 09:25:08 +0200

    Prime prove con print

---
+++ primo.codice.py
@@ -0,0 +1,8 @@
+# Primo codice di prova su python
+
+print("Hello World!")
+print("Il programma di questo corso prevede",
+      " ...",
+      "lo studio del liguaggio python",
+      sep=' \n ',
+      end='!!!!')

d64634e552aed7922019df2cd311237c94c3f250
pipxy <polarbearclimatechange2021@protonmail.com>
2022-06-30 09:45:59 +0200

    Prove di definizione di variabili e stampa

--- primo.codice.py
+++ primo.codice.py
@@ -6,3 +6,16 @@ print("Il programma di questo corso prevede",
       "lo studio del liguaggio python",
       sep=' \n ',
       end='!!!!')
+
+# Prove di definizione variabili
+
+message = 'Questo corso sembra parecchio interessante'
+n = 33.3
+pi = 3.141592653589793
+print(message)
+print(message, '--->', type(message))
+print('n ->', n, type(n))
+print(pi, '--->', type(pi))
+print(pi * n)
+n = 33.3 + pi
+print('n->', n, type(n))

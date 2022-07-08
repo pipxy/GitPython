f354cc5c76e5b60cb0cd7ed0a3931fc862813dff
pipxy <polarbearclimatechange2021@protonmail.com>
2022-07-08 10:09:51 +0200

    Corretto esercizio 1: compreso caso di lista vuota

--- 2lab/1_esercizio.py
+++ 2lab/1_esercizio.py
@@ -9,13 +9,12 @@ while nuovo_n != -1:
     print("Ecco la nuova lista: ", lista_N)
     nuovo_n = int(input("Inserisci un numero intero "))
 else:
-    print()
-    print()
-    print()
-    print("ERROR 404, hai inserito -1 !!!")
-    print()
-    print("La somma dei numeri inseriti è: ", sum(lista_N))
-    print("La media  dei numeri inseriti è: ", sum(lista_N)/len(lista_N))
-    print("Il massimo tra i numeri inseriti è: ",max(lista_N))
-    print("Il minimo tra i numeri inseriti è: ",min(lista_N))
-    print()
+    if len(lista_N) == 0:
+        print() ; print("Hai creato una lista vuota. Non posso operare con una lista vuota, sorry !") ; print()
+    else:
+         print() ; print() ; print()
+         print("ERROR 404, hai inserito -1 !!!") ; print()
+         print("La somma dei numeri inseriti è: ", sum(lista_N))
+         print("La media  dei numeri inseriti è: ", sum(lista_N)/len(lista_N))
+         print("Il massimo tra i numeri inseriti è: ", max(lista_N))
+         print("Il minimo tra i numeri inseriti è: ", min(lista_N)) ; print()
